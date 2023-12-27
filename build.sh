4c45aabaf5e98699235b56ad6fab791893996f6d
anan <ananclub@gmail.com>
2023-12-27 17:04:42 +0800

    static build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 TAG=${1:-"latest"}
-NAME=uvss5
+NAME=ss5uv
 DOCKERREPO="ananclub"
+rm -rf build
 mkdir -p build && cd build
 cmake .. && make
 
+exit 0
 cat > Dockerfile <<EOF
 FROM ubuntu:22.04
 ADD src/s5proxy /
